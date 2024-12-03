a31d53a740106eacbe70416abf73a96b7023555e
eli <john.berengu@student.moringaschool.com>
2024-12-03 23:14:50 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+// // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+const destructivelyAppendCat = (name) => {
+    cats.push ("Ralph");
+    return name
+};
+
+
+const destructivelyPrependCat = () => {
+    cats.unshift ("Bob");
+    
+};
+
+const destructivelyRemoveLastCat = () => {
+    cats.pop ();
+    
+};
+const destructivelyRemoveFirstCat = () => {
+    cats.shift ()
+};
+
+const appendCat = (name) => {
+  return [...cats,name];
+};
+
+const prependCat = (name) => {
+    return [name,...cats];
+};
+
+const removeLastCat = () => {
+    return cats.slice (0,-1);
+};
+
+const removeFirstCat = () => {
+    return cats.slice (1);
+};
+
+
+
+
+
+
+
